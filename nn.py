fa9dd3e0f02e5fe6c0e733f9a0217b56105afcf9
shashank524 <saishashank524@gmail.com>
2023-11-22 18:14:30 -0500

    new contructor

--- nn.py
+++ nn.py
@@ -17,7 +17,6 @@ import random
 import numpy as np
 
 class Network(object):
-
     def __init__(self, sizes):
         """The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
@@ -34,6 +33,22 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def __init__(self, sizes, biases, weights):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = biases
+        self.weights = weights
 
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
