b63dce37ea23977f2da79256d94971e201589d9b
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-09-21 01:24:37 -0400

    added random walk with updates

--- random_walk.py
+++ random_walk.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def random_walk(n, start, transition_matrix, steps):
+def simple_random_walk(n, start, transition_matrix, steps):
     result = [start]
     choicer = []
     for i in range(n):
@@ -8,5 +8,38 @@ def random_walk(n, start, transition_matrix, steps):
     for i in range(steps):
         choice = np.random.choice(choicer, p = transition_matrix[start])
         result.append(choice)
+        start = choice
+    return result
+
+WINDOW_SIZE = 10
+
+# define a distance metric to penalize jumps close to each other
+# dont want to skip i->j and i+1->j+1 etc
+def update(i, j, k, l, p_kl):
+    # now need to convert to prob
+    # TODO: test with other ways to convert except for sigmoid
+    euclidean_dist = np.sqrt((k - i) * (k - i) + (l - j) * (l - j))
+    return max(0, p_kl - np.sigmoid(euclidean_dist))
+
+def valid(i, j, n, m):
+    return i >= 0 and j >= 0 and i < n and j < m
+    
+def random_walk_with_updates(n, start, transition_matrix, steps):
+    result = [start]
+    choicer = [x for x in range(n)]
+    N = len(transition_matrix)
+    M = len(transition_matrix[0])
+    for _ in range(steps):
+        choice = np.random.choice(choicer, p = transition_matrix[start])
+        result.append(choice)
+        edge = [start, choice]
+        for c1 in range(-WINDOW_SIZE, WINDOW_SIZE + 1):
+            for c2 in range(-WINDOW_SIZE, WINDOW_SIZE + 1):
+                i = edge[0]
+                k = i + c1
+                j = edge[1]
+                l = j + c2
+                if valid(k, l, N, M):
+                    transition_matrix[k][l] = update(i, j, k, l, transition_matrix[k][l])
         start = choice
     return result
